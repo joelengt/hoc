b994b323db625d2fbc3f8da5411ed1756ba332d4
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-04-09 08:16:04 -0500

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,55 @@
+// example 1 
+function setIntervalHOC(WrappedComponent) {
+	return class WithSetInterval extends Component {
+		componentWillMount() {
+			this.intervals = [];
+		}
+
+		setInterval(...args) {
+			const id = setInterval.apply(null, args);
+			this.intervals.push(id);
+			return id;
+		}
+
+		componentWillUnmount() {
+			this.intervals.forEach(clearInterval);
+		}
+
+		render() {
+			return (
+				<WrappedComponent
+					setInterval={ this.setInterval.bind(this) }
+					{...this.props}
+				/>
+			)
+		}
+	}
+}
+
+// example 2
+const hightOrderComponent = (WrappedComponent) => {
+	class HOC extends React.Component {
+		componentWillMount() {
+			this.intervals = [];
+		}
+
+		setInterval(...args) {
+			const id = setInterval.apply(null, args);
+			this.intervals.push(id);
+			return id;
+		}
+
+		componentWillUnmount() {
+			this.intervals.forEach(clearInterval);
+		}
+
+		render() {
+			return (
+				<WrappedComponent
+					setInterval={ this.setInterval.bind(this) }
+					{...this.props}
+				/>
+			)
+		}
+	}
+}
