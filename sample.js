6fa1d6e98e4850f4d344dac727986ba4ed9c5c1b
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-04-11 07:54:51 -0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,66 +1,65 @@
 function getHOC(MyPrettyComponent) {
-	return function GetHOC(props) {
-		return <MyPrettyComponent getViewport={getViewport} {...props} />;
-	};
+  return function GetHOC(props) {
+    return <MyPrettyComponent getViewport={getViewport} {...props} />;
+  };
 }
 
 // example 1
 function setIntervalHOC(WrappedComponent) {
-	return class WithSetInterval extends Component {
-		componentWillMount() {
-			this.intervals = [];
-		}
+  return class WithSetInterval extends Component {
+    componentWillMount() { 
+      this.intervals = [];
+    }
 
-		setInterval(...args) {
-			const id = setInterval.apply(null, args);
-			this.intervals.push(id);
-			return id;
-		}
+    setInterval(...args) {
+     const id = setInterval.apply(null, args);
+     this.intervals.push(id);
+     return id;
+    }
 
-		componentWillUnmount() {
-			this.intervals.forEach(clearInterval);
-		}
+    componentWillUnmount() {
+      this.intervals.forEach(clearInterval);
+    }
 
-		render() {
-			return (
-				<WrappedComponent
-					setInterval={this.setInterval.bind(this)}
-					{...this.props}
-				/>
-			);
-		}
-	};
+    render() {
+      return (
+        <WrappedComponent
+          setInterval={this.setInterval.bind(this)}
+            {...this.props}
+        />
+      );
+    }
+  };
 }
 
 // example 2
 const hightOrderComponent = WrappedComponent => {
-	class HOC extends React.Component {
-		componentWillMount() {
-			this.intervals = [];
-		}
+  class HOC extends React.Component {
+    componentWillMount() {
+      this.intervals = [];
+    }
 
-		setInterval(...args) {
-			const id = setInterval.apply(null, args);
-			this.intervals.push(id);
-			return id;
-		}
+    setInterval(...args) {
+      const id = setInterval.apply(null, args);
+      this.intervals.push(id);
+      return id;
+    }
 
-		componentWillUnmount() {
-			this.intervals.forEach(clearInterval);
-		}
+    componentWillUnmount() {
+      this.intervals.forEach(clearInterval);
+    }
 
-		render() {
-			return (
-				<WrappedComponent
-					setInterval={this.setInterval.bind(this)}
-					{...this.props}
-				/>
-			);
-		}
-	}
-	
-	return HOC
+    render() {
+      return (
+        <WrappedComponent
+          setInterval={this.setInterval.bind(this)}
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  return HOC
 };
-
 
 
